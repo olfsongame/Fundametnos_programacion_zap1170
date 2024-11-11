0e7239060b650bea043bdd1ec1e5fd12574d41c8
olfsongame <aronolfson43@GMAIL.COM>
2024-11-11 08:15:05 -0600

    P016_capturadejuegos
    tarea

---
+++ P008_punterosv2/P008_punterosv2.cpp
@@ -0,0 +1,58 @@
+
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <locale>
+#include <Windows.h>
+//Espacio para declarar funciones
+void imprimir(std::string* titulo, std::string* Autor, int* anio_Pub, int tamanio)
+{
+    for (int i = 0; i < tamanio; i++)
+    {
+        std::cout << "El titulo del juego numero " << i + 1 << " es: " << titulo[i] << std::endl;
+        std::cout << "El Autor del juego numero " << i + 1 << " es: " << Autor[i] << std::endl;
+        std::cout << "El año de publicación del juego numero " << i + 1 << " es: " << anio_Pub[i] << std::endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "en_MX.UTF-8");
+    //Crear punteros para almacenar juegos
+    std::string* titulo = NULL; 
+    std::string* Autor = NULL;
+    int* anio_Publicacion = NULL;
+    int tamanio = 0;
+    std::cout << "Hola gamer cuantos juegos quieres guardar??\n";
+    std::cin >> tamanio;
+    //No tenemos que tener cautiva la memoria
+    
+    //Crear variables con el tamaño que deseamos
+    titulo = new std::string[tamanio];
+    Autor = new std::string[tamanio];
+    anio_Publicacion = new int[tamanio];
+    //metodo para almacenar información
+    for (int i = 0; i < tamanio; i++)
+    {
+        std::cout << "Ingresa el titulo del Juego número " << i + 1 << std::endl;
+        std::cin.ignore();
+        getline(std::cin, titulo[i]);
+        std::cout << "Ingresa el autor del Juego; " << std::endl;
+        getline(std::cin, Autor[i]);
+        std::cout << "Ingresa el año de publicación del Juego; " << std::endl;
+        std::cin >> anio_Publicacion[i];
+        system("CLS");
+    }
+    //Teoría/Practica de Funciones.
+    //Una función es una nave espacial
+    //Imprimir los datos recolectados
+
+    imprimir(titulo, Autor, anio_Publicacion, tamanio);
+    delete[] titulo;
+    delete[] Autor;
+    delete[] anio_Publicacion;
+    titulo = NULL;
+    Autor = NULL;
+    anio_Publicacion = NULL;
+}
